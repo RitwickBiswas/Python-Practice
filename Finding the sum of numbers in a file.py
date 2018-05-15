d5d5928eb821eac5104916817ddd4f68f75b551e
Ritwick Biswas <ritwick.biswas95@gmail.com>
2018-05-15 21:12:31 +0530

    Regex practice
    NIL

---
+++ Finding the sum of numbers in a file.py
@@ -0,0 +1,12 @@
+import re
+file = open("data.txt")
+num = []
+for line in file:
+    line = line.rstrip()
+    ext = re.findall("([0-9]+)",line)
+    if len(ext) < 1:
+        continue
+    for i in range(len(ext)):
+        number = float(ext[i])
+        num.append(number)
+print(sum(num))
